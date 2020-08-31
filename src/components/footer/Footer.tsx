77aee0d6f306456888ee2cfd7cacdd71cc4267ad
Frederik von Kiedrowski <frederik.kiedrowski@sevenval.com>
2020-08-31 21:55:58 +0200

    feat: add solial links to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './styles.scss';
 import { SocialList } from 'components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p>test</p>
       <SocialList />
+      <div className={styles.copyright}>
+        <FontAwesomeIcon icon={faCopyright} />
+        <div className={styles.text}>Frederik von Kiedrowski</div>
+      </div>
     </div>
   );
 };
--- src/components/socialList/SocialList.tsx
+++ src/components/socialList/SocialList.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import styles from './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faXing,
+  faStackOverflow,
+  faGithubAlt,
+} from '@fortawesome/free-brands-svg-icons';
 
 const SocialList = () => {
-  return <div className={styles.twitter}>Test</div>;
+  return (
+    <div className={styles.socialList}>
+      <a
+        target="_blanc"
+        href="https://github.com/Napster2709"
+        className={styles.github}
+      >
+        <FontAwesomeIcon icon={faGithubAlt} />
+      </a>
+      <a
+        target="_blanc"
+        href="https://www.xing.com/profile/Frederik_vonKiedrowski/cv"
+        className={styles.xing}
+      >
+        <FontAwesomeIcon icon={faXing} />
+      </a>
+      <a
+        target="_blanc"
+        href="https://stackoverflow.com/users/7581717/frederik-v-k"
+        className={styles.stackoverflow}
+      >
+        <FontAwesomeIcon icon={faStackOverflow} />
+      </a>
+    </div>
+  );
 };
 
 export { SocialList };
