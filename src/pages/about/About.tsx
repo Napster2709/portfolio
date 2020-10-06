4b2d986a6701a56222c9816ae50da19187f71c6f
Frederik von Kiedrowski <napstet@gmail.com>
2020-10-06 19:23:14 +0200

    refactor: change layout to more grid

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -8,36 +8,41 @@ const About = (): React.ReactElement => {
   const context = useContext(MainContext);
   return (
     <div className={styles.section}>
-      <Grid fluid>
-        <Grid.Column cols={5} className={cn(styles.about)}>
-          <h1 className={cn(styles.title, styles.slideLeft)}>About Me</h1>
-          <Line className={cn(styles.line, styles.slideRight)} align="right" />
-          <h3 className={styles.slideLeft}>
-            i&apos;m Frederik R. von Kiedrowski
-          </h3>
-        </Grid.Column>
-        <Grid.Column cols={7} className={cn(styles.text, styles.slideRight)}>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
-            distinctio sapiente possimus esse eius voluptates rerum optio unde,
-            deserunt in. Veniam, amet? Illum ducimus praesentium placeat
-            explicabo itaque unde reprehenderit. Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Dicta distinctio sapiente possimus
-            esse eius voluptates rerum optio unde, deserunt in. Veniam, amet?
-            Illum ducimus praesentium placeat explicabo itaque unde
-            reprehenderit. Lorem, ipsum dolor sit amet consectetur adipisicing
-            elit. Dicta distinctio sapiente possimus esse eius voluptates rerum
-            optio unde, deserunt in. Veniam, amet? Illum ducimus praesentium
-            placeat explicabo itaque unde reprehenderit. Lorem, ipsum dolor sit
-            amet consectetur adipisicing elit. Dicta distinctio sapiente
-            possimus esse eius voluptates rerum optio unde, deserunt in. Veniam,
-            amet? Illum ducimus praesentium placeat explicabo itaque unde
-            reprehenderit. Lorem, ipsum dolor sit amet consectetur adipisicing
-            elit. Dicta distinctio sapiente possimus esse eius voluptates rerum
-            optio unde, deserunt in. Veniam, amet?
-          </p>
-        </Grid.Column>
-      </Grid>
+      <div className={styles.centered}>
+        <Grid fluid>
+          <Grid.Column cols={5} className={cn(styles.about)}>
+            <h1 className={cn(styles.title, styles.slideLeft)}>About Me</h1>
+            <Line
+              className={cn(styles.line, styles.slideRight)}
+              align="right"
+            />
+            <h3 className={styles.slideLeft}>
+              i&apos;m Frederik R. von Kiedrowski
+            </h3>
+          </Grid.Column>
+          <Grid.Column cols={7} className={cn(styles.text, styles.slideRight)}>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              distinctio sapiente possimus esse eius voluptates rerum optio
+              unde, deserunt in. Veniam, amet? Illum ducimus praesentium placeat
+              explicabo itaque unde reprehenderit. Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Dicta distinctio sapiente possimus
+              esse eius voluptates rerum optio unde, deserunt in. Veniam, amet?
+              Illum ducimus praesentium placeat explicabo itaque unde
+              reprehenderit. Lorem, ipsum dolor sit amet consectetur adipisicing
+              elit. Dicta distinctio sapiente possimus esse eius voluptates
+              rerum optio unde, deserunt in. Veniam, amet? Illum ducimus
+              praesentium placeat explicabo itaque unde reprehenderit. Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Dicta
+              distinctio sapiente possimus esse eius voluptates rerum optio
+              unde, deserunt in. Veniam, amet? Illum ducimus praesentium placeat
+              explicabo itaque unde reprehenderit. Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Dicta distinctio sapiente possimus
+              esse eius voluptates rerum optio unde, deserunt in. Veniam, amet?
+            </p>
+          </Grid.Column>
+        </Grid>
+      </div>
     </div>
   );
 };
