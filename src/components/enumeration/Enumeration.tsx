788cff34adfaf0e928401ec81203271b365acfdc
Frederik von Kiedrowski <napstet@gmail.com>
2021-04-05 15:17:32 +0200

    feat: simulate infinite scrolling list

--- src/components/enumeration/Enumeration.tsx
+++ src/components/enumeration/Enumeration.tsx
@@ -11,6 +11,10 @@ const Enumeration = ({ items }: EnumerationProps): React.ReactElement => {
   const itemRef = React.createRef<HTMLDivElement>();
 
   useEffect(() => {
+    items.push(items[0]);
+  }, []);
+
+  useEffect(() => {
     const interval = setInterval(() => {
       setIter(iter < items.length - 1 ? iter + 1 : 0);
     }, 2000);
@@ -21,18 +25,25 @@ const Enumeration = ({ items }: EnumerationProps): React.ReactElement => {
     setHeight(itemRef.current.clientHeight / items.length);
   }, [itemRef]);
 
+  const style =
+    iter !== 0
+      ? {
+          transform: `translateY(-${height * iter}px)`,
+          transition: 'transform 0.5s cubic-bezier(0.78, 0.2, 0.15, 1.01)',
+        }
+      : { transform: `translateY(-${height * iter}px)` };
+
   return (
     <div className={styles.container}>
-      <div
-        className={styles.itemBox}
-        ref={itemRef}
-        style={{ transform: `translateY(-${height * iter}px)` }}
-      >
-        {items.map((item, index) => (
-          <div className={styles.item} key={index}>
-            {item}
-          </div>
-        ))}
+      <div className={styles.itemBox} ref={itemRef} style={style}>
+        {items.map(
+          (item, index) =>
+            item && (
+              <div className={styles.item} key={index}>
+                {item}
+              </div>
+            ),
+        )}
       </div>
     </div>
   );
