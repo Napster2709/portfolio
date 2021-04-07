67f3d93c1bc97f9f5693c7ce403b0fb576aa541e
Frederik von Kiedrowski <napstet@gmail.com>
2021-04-07 21:36:31 +0200

    feat: add meta describtion / layout fix

--- src/pages/onePager/OnePager.tsx
+++ src/pages/onePager/OnePager.tsx
@@ -3,6 +3,7 @@ import styles from './styles.scss';
 import {
   ColorThemeToggle,
   Enumeration,
+  Grid,
   Huge,
   Line,
   Logo,
@@ -13,33 +14,37 @@ import cn from 'classnames';
 const OnePager = (): React.ReactElement => {
   return (
     <Section className={styles.height100}>
-      <ColorThemeToggle className={styles.toggle} />
-      <div className={cn(styles.content)}>
-        <div className={styles.titleSection}>
-          <Logo className={cn(styles.logo)} />
-          <Line svg className={cn(styles.line)} />
-          <div className={cn(styles.titleText, styles.slideRight)}>
-            <div className={styles.meta}>Hello</div>
-            <Huge className={styles.title}>I&lsquo;m Frederik</Huge>
+      <Grid className={cn(styles.content)}>
+        <Grid.Column className={styles.toggleContainer}>
+          <ColorThemeToggle className={styles.toggle} />
+        </Grid.Column>
+        <Grid.Column className={styles.titleContainer}>
+          <div className={styles.titleSection}>
+            <Logo className={cn(styles.logo)} />
+            <Line svg className={cn(styles.line)} />
+            <div className={cn(styles.titleText, styles.slideRight)}>
+              <div className={styles.meta}>Hello</div>
+              <Huge className={styles.title}>I&lsquo;m Frederik</Huge>
+            </div>
           </div>
-        </div>
-        <div
-          className={cn(styles.text, styles.slideUp, styles.textAlignCenter)}
-        >
-          I&lsquo;m a Software Developer with a liking in
-          <Enumeration
-            className={styles.list}
-            items={[
-              'Design',
-              'Clean Code',
-              'Modern languages',
-              'coffee',
-              'Gaming',
-              'Music',
-            ]}
-          />
-        </div>
-      </div>
+          <div
+            className={cn(styles.text, styles.slideUp, styles.textAlignCenter)}
+          >
+            I&lsquo;m a Software Developer with a liking in
+            <Enumeration
+              className={styles.list}
+              items={[
+                'Design',
+                'Clean Code',
+                'Modern languages',
+                'coffee',
+                'Gaming',
+                'Music',
+              ]}
+            />
+          </div>
+        </Grid.Column>
+      </Grid>
     </Section>
   );
 };
