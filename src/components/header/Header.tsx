7970532c7a5749147722d20360907593397d6e1f
Freddy <napstet@gmail.com>
2021-10-08 23:14:58 +0200

    feat: impress changes / color tweeks

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,22 +2,16 @@ import React from 'react';
 import { Logo } from '../logo';
 import styles from './styles.scss';
 import cn from 'classnames';
-import { BackArrow, ColorThemeToggle, Grid } from 'components';
+import { ColorThemeToggle, Grid } from 'components';
 import { Link } from 'react-router-dom';
 
 interface HeaderProps {
   logo?: boolean;
-  back?: string;
 }
 
-const Header = ({ logo, back }: HeaderProps): React.ReactElement => {
+const Header = ({ logo }: HeaderProps): React.ReactElement => {
   return (
     <>
-      {back && (
-        <div className={styles.back}>
-          <BackArrow to={back} />
-        </div>
-      )}
       <div className={cn(styles.header, styles.fadeIn)}>
         <Grid className={styles.content}>
           {logo && (
--- src/pages/imprint/imprint.tsx
+++ src/pages/imprint/imprint.tsx
@@ -1,4 +1,4 @@
-import { Grid, Header, Line, PrivatText, Section } from 'components';
+import { BackArrow, Grid, Header, PrivatText, Section } from 'components';
 import React from 'react';
 import styles from './styles.scss';
 import cn from 'classnames';
@@ -6,58 +6,34 @@ import cn from 'classnames';
 const Imprint = (): React.ReactElement => {
   return (
     <>
-      <Header logo back="/" />
+      <Header logo />
       <div className={styles.main}>
-        <Section marginTop="5rem" marginBottom="5rem">
+        <Grid className={styles.nav}>
+          <Grid.Column cols={6}>
+            <div className={styles.slideRight}>
+              <h1>Imprint</h1>
+            </div>
+          </Grid.Column>
+          <Grid.Column cols={6}>
+            <BackArrow to="/" />
+          </Grid.Column>
+        </Grid>
+
+        <Section className={cn(styles.box, styles.slideUp)}>
           <Grid fluid>
-            <Grid.Column
-              centered
-              cols={5}
-              className={cn(styles.title, styles.slideLeft)}
-            >
-              <div className={styles.content}>
-                <h1>Contact</h1>
-              </div>
-            </Grid.Column>
-            <Grid.Column centered cols={2}>
-              <Line svg className={styles.line} />
-            </Grid.Column>
-            <Grid.Column
-              cols={5}
-              centered
-              className={cn(styles.text, styles.slideRight)}
-            >
+            <Grid.Column marginBottom="1rem">
+              <h2>Contact</h2>
               <div className={styles.content}>
                 <PrivatText type="name" />
                 <PrivatText type="email" />
               </div>
             </Grid.Column>
-          </Grid>
-        </Section>
-
-        <Section marginTop="5rem" marginBottom="5rem">
-          <Grid fluid className={styles.address}>
-            <Grid.Column
-              cols={5}
-              centered
-              className={cn(styles.title, styles.slideLeft)}
-            >
+            <Grid.Column>
+              <h2>Address</h2>
               <div className={styles.content}>
                 <PrivatText type="street" />
                 <PrivatText type="city" />
                 <PrivatText type="land" />
-              </div>
-            </Grid.Column>
-            <Grid.Column centered cols={2}>
-              <Line svg className={styles.line2} />
-            </Grid.Column>
-            <Grid.Column
-              cols={5}
-              centered
-              className={cn(styles.text, styles.slideRight)}
-            >
-              <div className={styles.content}>
-                <h1>Address</h1>
               </div>
             </Grid.Column>
           </Grid>
