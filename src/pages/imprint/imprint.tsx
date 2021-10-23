c45dc986a587940009701e260e79bfce1a91eb8f
Freddy <napstet@gmail.com>
2021-10-23 20:36:53 +0200

    refactor: change imprint styling

--- src/pages/imprint/imprint.tsx
+++ src/pages/imprint/imprint.tsx
@@ -2,6 +2,9 @@ import { BackArrow, Grid, Header, PrivatText, Section } from 'components';
 import React from 'react';
 import styles from './styles.scss';
 import cn from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import { faMailBulk, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Imprint = (): React.ReactElement => {
   return (
@@ -21,19 +24,32 @@ const Imprint = (): React.ReactElement => {
 
         <Section className={cn(styles.box, styles.slideUp)}>
           <Grid fluid>
-            <Grid.Column marginBottom="1rem">
-              <h2>Contact</h2>
-              <div className={styles.content}>
-                <PrivatText type="name" />
-                <PrivatText type="email" />
+            <Grid.Column className={styles.inner} marginBottom="4rem">
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon className={styles.icon} icon={faMailBulk} />
+              </div>
+              <div>
+                <h2>Contact</h2>
+                <div className={styles.content}>
+                  <PrivatText type="name" />
+                  <PrivatText type="email" />
+                </div>
               </div>
             </Grid.Column>
-            <Grid.Column>
-              <h2>Address</h2>
-              <div className={styles.content}>
-                <PrivatText type="street" />
-                <PrivatText type="city" />
-                <PrivatText type="land" />
+            <Grid.Column className={styles.inner}>
+              <div className={styles.iconContainer}>
+                <FontAwesomeIcon
+                  className={styles.icon}
+                  icon={faMapMarkedAlt}
+                />
+              </div>
+              <div>
+                <h2>Address</h2>
+                <div className={styles.content}>
+                  <PrivatText type="street" />
+                  <PrivatText type="city" />
+                  <PrivatText type="land" />
+                </div>
               </div>
             </Grid.Column>
           </Grid>
